77957d7c7c140386a66029cc7bf64975f8049cdb
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-12 19:12:26 +0100

    boolean practice

---
+++ ifV.py
@@ -0,0 +1,7 @@
+friends = int(input(" How much friends ? "))
+money = int(input(" How much money you hawe ? "))
+icecream_price = int(input(" Price of the icecream "))
+if friends*icecream_price <= money:
+    print(" Lets taste icecream !")
+else:
+    print(" Another time ):")
