6d7d5562052b9f3be233a1b454225e7a9db41ffc
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-13 20:07:02 +0100

    just practice with functions

---
+++ kalk.py
@@ -0,0 +1,15 @@
+num1 = int(input(" Number 1 "))
+num2 = int(input(" Number 2 "))
+fun = (input(" Function "))
+res1 = num1 / num2
+res2 = num1 * num2
+res3 = num1 + num2
+res4 = num1 - num2
+if num1 / num2:
+    print(res1)
+if num1 * num2:
+    print(res2)
+if num1 + num2:
+    print(res3)
+if num1 - num2:
+    print(res4)
