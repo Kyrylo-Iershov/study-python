82cbe262bf3c9a0b1d66d89238df2988f7b0ba9f
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-15 18:36:41 +0100

    car/garage

---
+++ box with bolls.py
@@ -0,0 +1,9 @@
+color = input("Car color :")
+if color == "green":
+    print(" Garage 1 ")
+elif color == "black":
+    print(" Garage 2 ")
+elif color == "red":
+    print(" Garage 3 ")
+else:
+    print(" Garage 4 ")
