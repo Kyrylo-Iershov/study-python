f5091bb2b60a8a1a1d33c368c01489ae1efe6dce
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-16 20:52:34 +0100

    un/dis

---
+++ 1/pr.py
@@ -0,0 +1,5 @@
+name = input(" Enter your name : ")
+sem_1_score = int(input(" 1 semester score : "))
+sem_2_score = int(input(" 2 semester score : "))
+if (sem_1_score + sem_2_score)/2 > 90 < 100:
+    print(" Your discount 50%, Congratulation ! ")
