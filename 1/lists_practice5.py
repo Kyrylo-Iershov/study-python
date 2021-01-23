09c4d00bd7cc42148756f890474291e0d2129329
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-23 20:23:35 +0100

    lists_practice5_v2

--- 1/lists_practice5.py
+++ 1/lists_practice5.py
@@ -2,3 +2,5 @@ queue = ["John", "James", "Adam", "Adrian", "Amy", "Ali"]
 txt = input(" Input custumer name : ")
 queue.append(txt)
 print(queue)
+num = int(input(" Input custumer number : "))
+print(queue[num])
