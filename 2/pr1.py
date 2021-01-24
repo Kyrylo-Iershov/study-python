6f2d6cf65b8faab549a4fedf1890624f90757421
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-24 20:09:09 +0100

    pr1_v2

--- 2/pr1.py
+++ 2/pr1.py
@@ -1,7 +1,13 @@
 x = 1
-while x < 99:
+while x < 5:
     if x%2 == 0:
         print(str(x) + " is even ")
     else:
         print(str(x) + " is odd ")
     x+=1
+n = int(input())
+lenght = 0
+while n > 0:
+    n//=10
+    lenght +=1
+print(lenght)
