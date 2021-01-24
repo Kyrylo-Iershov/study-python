613eea4c791a429db6cc57767590b69a21135810
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-24 19:39:31 +0100

    lists_practice6_v3

--- 1/list_practice6.py
+++ 1/list_practice6.py
@@ -1,8 +1,9 @@
 words = ["Python", "fun"]
 index = 1
-words.insert(index,"is")
+words.insert(index, "is")
 words.append("!")
 print(words)
 numbers = [14, 5, 6, 8, 17, 28, 55]
-print(max(numbers))
-print(min(numbers))
+max1 = max(numbers)
+min1 = min(numbers)
+print(max1 + min1)
