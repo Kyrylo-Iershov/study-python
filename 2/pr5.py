e02713f5b0a9535439bb5026f97f2c5cf4d1b299
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-26 20:36:08 +0100

    pr5_v2

--- 2/pr5.py
+++ 2/pr5.py
@@ -2,3 +2,6 @@ words = ["spam", "eggs", "python", "tutorial", "is", "fun"]
 index = 1
 words.insert(index, "and")
 print(words)
+words = ["spam", "eggs", "python", "tutorial", "is", "fun"]
+for word in words:
+    print(word + "!")
