e11b7a9fed302762b296d0ea364f20287f13a7d2
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-01-26 20:47:12 +0100

    pr5_v3

--- 2/pr5.py
+++ 2/pr5.py
@@ -5,3 +5,9 @@ print(words)
 words = ["spam", "eggs", "python", "tutorial", "is", "fun"]
 for word in words:
     print(word + "!")
+str = "testing for loops"
+count = 0
+for x in str:
+    if(x == 't'):
+        count += 1
+print(count)
