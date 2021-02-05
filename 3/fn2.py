da908942bf3407a6200bf54e01993d662933df0a
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-02-05 11:10:47 +0100

    fn2_v.1.1

--- 3/fn2.py
+++ 3/fn2.py
@@ -1,10 +1,14 @@
-password = input(" Input yuor password : ")
+password = input(" Input your password : ")
 confirm = input(" Confirm your password : ")
 text1 = list(password)
 text2 = list(confirm)
+
+
 def correct_password():
     if text1 == text2:
         print("Correct ! ")
     elif text1 != text2:
         print(" Incorrect, try again !")
+
+
 correct_password()
