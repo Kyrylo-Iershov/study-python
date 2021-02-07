bb81c9435e1d580a62f9dbad87950e9023fe2af1
Kyrylo-Iershov <data.mail.wild@gmail.com>
2021-02-07 16:43:25 +0100

    fn6_v.1.0

---
+++ 3/fn6.py
@@ -0,0 +1,11 @@
+import random
+
+for i in range(5):
+    value = random.randint(1, 6)
+    print(value)
+import math
+num = 10
+print(math.sqrt(num))
+from math import pi
+print(pi)
+# practice with mudules
